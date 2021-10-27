78585fc6fdbc99f088ac0b4dad9772f6128a2241
Анастасия Оленёва <oleneva1995@icloud.com>
2021-10-27 21:49:33 +0300

    Добавьте файлы проекта.

---
+++ Lab 8.2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lab_8._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "Log/Log.txt";
+
+            if (Directory.Exists(path))
+            {
+
+                File.Create(path);
+            }
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                Random rnd = new Random();
+                for (int i = 1; i <= 10; i++)
+                {
+                    sw.WriteLine(rnd.Next(100));
+
+                }
+
+
+            }
+            using (StreamReader sr = new StreamReader(path))
+            {
+                int sum = 0;
+                
+                while (sr.Peek()>-1)
+                {
+                    int number = Convert.ToInt32(sr.ReadLine());
+
+                    sum += number;
+                }
+
+
+                Console.WriteLine("сумма чисел равна {0}",sum);
+         
+            }
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
